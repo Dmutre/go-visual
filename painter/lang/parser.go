b46888a178fd96660d7ea077caabfce403971916
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-04-24 21:35:51 +0200

    Update parser.go

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,21 +1,60 @@
 package lang
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"strings"
 
-	"github.com/roman-mazur/architecture-lab-3/painter"
+	"github.com/Dmutre/go-visual/painter"
 )
 
-// Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
-type Parser struct {
+type Parser struct{}
+
+func complexOperation(operation painter.OperationFunc) painter.Operation {
+	if operation == nil {
+		return nil
+	}
+	return painter.OperationFunc(operation)
+}
+
+func (p *Parser) CommandParser(commandName string, args []string) painter.Operation {
+	fmt.Println(commandName, args)
+	switch commandName {
+	case "white":
+		return painter.OperationFunc(painter.WhiteFill)
+	case "green":
+		return painter.OperationFunc(painter.GreenFill)
+	case "bgrect":
+		return complexOperation(painter.DrawRectangle(args))
+	case "move":
+		return complexOperation(painter.Move(args))
+	case "figure":
+		return complexOperation(painter.Figure(args))
+	case "update":
+		return painter.UpdateOp
+	case "reset":
+		return painter.OperationFunc(painter.Reset)
+	}
+	return nil
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 	var res []painter.Operation
 
-	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
 
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+
+		sliced := strings.Split(commandLine, " ")
+		args := sliced[1:]
+
+		command := p.CommandParser(sliced[0], args)
+		if command != nil {
+			res = append(res, command)
+		}
+	}
 	return res, nil
 }
